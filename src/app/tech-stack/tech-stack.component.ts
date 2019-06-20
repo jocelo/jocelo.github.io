7153a9ab08fd5db2a9f89034402d6b27a02d6134
alfredo.alonso <alfredo.alonso@prodapt.com>
2019-06-20 12:00:59 -0400

    Time slots removed

--- src/app/tech-stack/tech-stack.component.ts
+++ src/app/tech-stack/tech-stack.component.ts
@@ -9,6 +9,7 @@ import { faCoffee, faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
 export class TechStackComponent implements OnInit {
   faAngleDoubleDown = faAngleDoubleDown;
   techstack = [];
+  data = [];
   timelapse = {};
   startTime = 0;
   endTime = 0;
@@ -33,31 +34,30 @@ export class TechStackComponent implements OnInit {
       });
     }
 
-    this.techstack = [{
+    this.data = [{
       title: 'Frontend',
       details: [{
         title: 'javascript',
         details: [{
           title: 'react',
           icon: '',
-          time: [{
-            start: '10/01/2007',
-            end: '12/12/2015'
-          }]
+          start: '09/01/2018',
+          end: '06/30/2019'
         },{
           title: 'angular',
           icon: '',
-          time: [{
             start: '1/15/2019',
             end: ''
-          }]
+        },{
+          title: 'vue',
+          icon: '',
+          start: '07/01/2017',
+          end: '08/30/2018'
         },{
           title: 'jQuery',
           icon: '',
-          time: [{
-            start: '1/15/2019',
-            end: ''
-          }]
+          start: '06/01/2017',
+          end: '06/30/2019'
         }]
       }]
     },{
@@ -65,21 +65,63 @@ export class TechStackComponent implements OnInit {
       details: [{
         title: 'python',
         icon: '',
-        time: [{
-          start: '1/1/2010',
-          end: '12/31/2013'
-        }]
+        start: '10/1/2015',
+        end: '07/01/2017'
+      },{
+        title: 'php',
+        icon: '',
+        start: '10/1/2015',
+        end: '07/01/2017'
+      },{
+        title: 'coldfusion',
+        icon: '',
+        start: '10/1/2015',
+        end: '06/30/2017'
       }]
     },{
       title: 'Databases',
-      details: []
+      details: [{
+        title: 'SQL Server',
+        icon: '',
+        start: '10/1/2015',
+        end: '06/30/2017'
+      }]
     },{
-      title: 'Softskills',
-      details: []
+      title: 'Devops',
+      details: [{
+        title: 'docker',
+        icon: '',
+        start: '07/01/2017',
+        end: '08/30/2018'
+      }]
     }];
   }
 
   ngOnInit() {
+    this.techstack = this.data.map((category)=>{
+      const allMinDates = this.getAllDates([], 'start', category.details);
+      const allMaxDates = this.getAllDates([], 'end', category.details);
+      const minDate = allMinDates.reduce((lowestDateStr, actualDateStr)=> new Date(lowestDateStr).getTime() > new Date(actualDateStr).getTime()  ? actualDateStr : lowestDateStr, allMinDates[0]);
+      const maxDate = allMaxDates.reduce((greatestDateStr, actualDateStr)=> new Date(greatestDateStr).getTime() > new Date(actualDateStr).getTime()  ? actualDateStr : greatestDateStr, allMaxDates[0]);
+
+      return ({
+        title: category.title,
+        startDate: new Date(minDate),
+        endDate: new Date(maxDate),
+        parent: null
+      });
+    });
+    console.log('this.techstack', this.techstack);
   }
 
+  getAllDates(allDates: any[], dateType: string, details: any[]) {
+    for (var i=0 ; i<details.length ; i++) {
+      if (details[i]['details']) {
+        allDates.concat(this.getAllDates(allDates, dateType, details[i].details));
+      }
+
+      allDates.push(details[i][dateType]);
+    }
+    return allDates;
+  }
 }

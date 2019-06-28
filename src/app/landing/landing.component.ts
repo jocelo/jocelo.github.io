949597452b9139e1185b425a58312e40ac299ce1
jocelo <gopher@gmail.com>
2019-06-28 00:18:48 -0400

    Langing WIP

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   cochinada: string = '';
   shake: boolean = false;
-  topics: Array<string> = ['portfolio', 'certifications', 'games', 'photos', 'languages', 'store', 'tech stack', 'videos'];
+  topics: Array<string> = ['portfolio', 'certifications', 'photos', 'languages', 'store', 'tech stack', 'videos'];
   topicSelected: string;
 
   constructor() {
+    this.getRandomTopic();
+  }
+
+  getRandomTopic() {
     let selected = Math.floor(Math.random() * this.topics.length);
     this.topicSelected = this.topics[selected];
   }
@@ -27,9 +31,14 @@ export class LandingComponent implements OnInit {
   handleKeyDown(event): void {
     event.preventDefault();
     event.stopPropagation();
+
     if ( event.keyCode == 8) {
       this.cochinada = this.cochinada.slice(0,-1);
       this.killAnimation();
+      
+      if (this.cochinada.length === 0) {
+        this.getRandomTopic();
+      }
     } else if (this.cochinada.length < this.topicSelected.length) {
       this.cochinada += this.topicSelected[this.cochinada.length];
       this.killAnimation();

135cc6dec18689b1bfe8907fb58b4fc7dbd4e0d0
jocelo <gopher@gmail.com>
2019-11-16 08:42:23 -0500

    layout

--- src/app/layouts/blog/blog.component.ts
+++ src/app/layouts/blog/blog.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
-
+  posts = [];
   constructor() { }
 
   ngOnInit() {
+    this.posts = [
+      {id: 1, title: 'Basic css shapes Go'},
+      {id: 2, title: 'Searching for a Job in tech'},
+      {id: 3, title: 'Latin guy in tech'}
+    ];
   }
 
 }

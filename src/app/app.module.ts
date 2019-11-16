5eb60b92f16c9056d35a3908e9debaa449949ed6
jocelo <gopher@gmail.com>
2019-11-16 07:39:45 -0500

    Blog section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { CertificationsComponent } from './certifications/certifications.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { LandingComponent } from './landing/landing.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TechStackComponent } from './tech-stack/tech-stack.component';
-import { PersonalComponent } from './personal/personal.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { CertificationsComponent } from './layouts/certifications/certifications.component';
+import { PortfolioComponent } from './layouts/portfolio/portfolio.component';
+import { LandingComponent } from './layouts/landing/landing.component';
+import { PageNotFoundComponent } from './layouts/page-not-found/page-not-found.component';
+import { TechStackComponent } from './layouts/tech-stack/tech-stack.component';
+import { PersonalComponent } from './layouts/personal/personal.component';
+import { BlogComponent } from './layouts/blog/blog.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FooterComponent } from './footer/footer.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/all', pathMatch: 'full' },
   { path: 'index', redirectTo: '/all' },
   { path: 'all', component: PersonalComponent },
+  { path: 'blog', component: BlogComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: 'certifications', component: CertificationsComponent },
   { path: 'tech-stack', component: TechStackComponent },
@@ -35,7 +37,8 @@ const appRoutes: Routes = [
     PageNotFoundComponent,
     TechStackComponent,
     PersonalComponent,
-    FooterComponent
+    FooterComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,

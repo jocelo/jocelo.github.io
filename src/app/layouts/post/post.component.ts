bccbc1673fb617522a384c206f55e4f9b593d170
jocelo <gopher@gmail.com>
2019-11-18 07:31:49 -0500

    post details

--- src/app/layouts/post/post.component.ts
+++ src/app/layouts/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  currentID: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.currentID = this.route.snapshot.paramMap.get('id');
   }
 
 }

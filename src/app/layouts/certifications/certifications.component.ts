ba9208c4e9bdbda81e97f7bb411ed1cfcf460005
jocelo <gopher.ae@gmail.com>
2020-10-13 20:33:12 -0400

    new HR cert

--- src/app/layouts/certifications/certifications.component.ts
+++ src/app/layouts/certifications/certifications.component.ts
@@ -10,9 +10,16 @@ export class CertificationsComponent implements OnInit {
 
   constructor() {
     this.certs = [{
+      obtainedDate: 'October 13, 2020',
+      desc: 'Problem Solving (Basic)',
+      siteLink: 'https://www.hackerrank.com/jocelohere',
+      site: 'HackerRank',
+      certLink: 'https://www.hackerrank.com/certificates/3ec3b382b827',
+      certImage: 'https://res.cloudinary.com/mycollections/image/upload/v1602634136/certificates/hr_problem_solving_basic.png'
+    },{
       obtainedDate: 'October 12, 2020',
       desc: 'Python (Basic)',
-      siteLink: 'https://www.certmetrics.com/amazon/public/transcript.aspx?transcript=N3EDK54122EQ1DC3',
+      siteLink: 'https://www.hackerrank.com/jocelohere',
       site: 'HackerRank',
       certLink: 'https://www.hackerrank.com/certificates/ea625f254cf8',
       certImage: 'https://res.cloudinary.com/mycollections/image/upload/v1602616305/certificates/hr_python_basic.png'

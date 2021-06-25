6b3343f3c881c38517197bb2cfdcf733daad04da
Jocelo <jocelohere@gmail.com>
2021-06-25 09:47:41 -0400

    angular version upgraded to 7

--- src/app/layouts/portfolio/portfolio.component.ts
+++ src/app/layouts/portfolio/portfolio.component.ts
@@ -36,75 +36,64 @@ export class PortfolioComponent implements OnInit {
       desc: "Website created for sharing resources, videos and games of math topics. Built for helping elementary and mid school students and supporting teachers.",
       img: 'portfolio.png',
       techStack: [
-        { name: 'angular 8', icon: 'angular'},
-        { name: 'typescript', icon: 'code'},
-        { name: 'material design', icon: 'materialDesign'},
-        { name: 'nodeJs', icon: 'node'},
-        { name: 'heroku', icon: 'server'},
-        { name: 'aws', icon: 'aws'}
+        { name: 'angular 8', icon: 'angular' },
+        { name: 'typescript', icon: 'code' },
+        { name: 'material design', icon: 'materialDesign' },
+        { name: 'nodeJs', icon: 'node' },
+        { name: 'heroku', icon: 'server' },
+        { name: 'aws', icon: 'aws' }
       ]
-    },{
+    }, {
       title: 'Bhuvi IT Solutions Hiring Website',
       link: 'https://hiring.bhuviits.com/home',
       desc: "Bhuvi IT solutions platform for new candidates, where they can track the hiring process and have all the resources they need.",
       img: 'hiring.png',
       techStack: [
-        { name: 'angular 8', icon: 'angular'},
-        { name: 'typescript', icon: 'code'},
-        { name: 'github', icon: 'github'},
-        { name: 'nodeJs', icon: 'node'},
+        { name: 'angular 8', icon: 'angular' },
+        { name: 'typescript', icon: 'code' },
+        { name: 'github', icon: 'github' },
+        { name: 'nodeJs', icon: 'node' },
       ]
-    },{
-      title: 'Arafel International',
-      link: 'http://ysistemak.sytes.net:8081/BelcrilWebSite/public/',
-      desc: "Web Portal for Mexico localized shopping of clothing brand. Worked in the backend portion.",
-      img: 'arafel.png',
-      techStack: [
-        { name: 'php', icon: 'php'},
-        { name: 'laravel', icon: 'laravel'},
-        { name: 'mysql', icon: 'database'},
-        { name: 'svn', icon: 'svn'},
-      ]
-    },{
+    }, {
       title: 'JavaScript Mentor at Exercism Website',
       link: 'https://exercism.io/tracks/javascript/mentors',
       desc: "Non-profit project that help developer students to learn different languages. Working in JavaScript's language/track to mentor students in learning JavaScript's language.",
       img: 'exercism.png',
       techStack: [
-        { name: 'javascript', icon: 'js'},
-        { name: 'github', icon: 'github'}
+        { name: 'javascript', icon: 'js' },
+        { name: 'github', icon: 'github' }
       ]
-    },{
+    }, {
       title: 'Wordsearch Generator',
       link: 'https://the-wordsearch-game.herokuapp.com/',
       desc: "Work in progress Application to assist when generating wordsearch games. One of the main features is that it is miltilingual and in the plans it is to have export and import features. This games will be used to feed the crossword game that's being developed.",
       img: 'wordsearch.png',
       techStack: [
-        { name: 'angular 6', icon: 'angular'},
-        { name: 'typescript', icon: 'code'},
-        { name: 'font awesome', icon: 'fontAwesome'},
-        { name: 'nodeJs', icon: 'node'},
-        { name: 'heroku', icon: 'server'},
-        { name: 'firebase', icon: 'firebase'}
+        { name: 'angular 6', icon: 'angular' },
+        { name: 'typescript', icon: 'code' },
+        { name: 'font awesome', icon: 'fontAwesome' },
+        { name: 'nodeJs', icon: 'node' },
+        { name: 'heroku', icon: 'server' },
+        { name: 'firebase', icon: 'firebase' }
       ]
-    },{
+    }, {
       title: 'Snake Game',
       link: 'https://jocelo.github.io/snake-game/',
       desc: 'Simple vanilla JS game that replicates the funcionality of the snake game where you move a snake over a board with the only objective to eat apples.',
       img: 'snake.png',
       techStack: [
-        { name: 'javascript', icon: 'js'},
-        { name: 'github pages', icon: 'server'}
+        { name: 'javascript', icon: 'js' },
+        { name: 'github pages', icon: 'server' }
       ]
-    },{
+    }, {
       title: 'Credit Card Validator',
       link: 'https://public.jocelohere.now.sh/',
       desc: 'Small Project generated using svelte JS and serverless technology for deploying the project. Created to particiate in a challenge launched by coderos youtube channel.',
       img: 'ccvalidator.png',
       techStack: [
-        { name: 'svelte', icon: 'js'},
-        { name: 'nodeJs', icon: 'node'},
-        { name: 'zeit now', icon: 'server'}
+        { name: 'svelte', icon: 'js' },
+        { name: 'nodeJs', icon: 'node' },
+        { name: 'zeit now', icon: 'server' }
       ]
     }]
   }

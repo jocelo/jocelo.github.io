17f6d7e23e6da1444f11052ec62fe44aa26dbaa6
Jocelo <jocelohere@gmail.com>
2022-02-06 08:23:40 -0600

    lint changes

--- src/app/layouts/landing/landing.component.ts
+++ src/app/layouts/landing/landing.component.ts
@@ -8,8 +8,8 @@ import { faGithub, faCodepen, faLinkedin, faYoutube, faTwitter, faInstagram, faJ
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  wordSearched: string = '';
-  shake: boolean = false;
+  wordSearched = '';
+  shake = false;
   topics: Array<string> = ['portfolio', 'certifications', 'photos', 'languages', 'store', 'tech stack', 'videos'];
   topicSelected: string;
 
@@ -30,11 +30,11 @@ export class LandingComponent implements OnInit {
   }
 
   getRandomTopic() {
-    let selected = Math.floor(Math.random() * this.topics.length);
+    const selected = Math.floor(Math.random() * this.topics.length);
     this.topicSelected = this.topics[selected];
   }
-  
-  animate(status=true): void {
+
+  animate(status = true): void {
     this.shake = status;
   }
 
@@ -46,10 +46,10 @@ export class LandingComponent implements OnInit {
     event.preventDefault();
     event.stopPropagation();
 
-    if ( event.keyCode == 8) {
-      this.wordSearched = this.wordSearched.slice(0,-1);
+    if (event.keyCode == 8) {
+      this.wordSearched = this.wordSearched.slice(0, -1);
       this.killAnimation();
-      
+
       if (this.wordSearched.length === 0) {
         this.getRandomTopic();
       }
@@ -61,7 +61,7 @@ export class LandingComponent implements OnInit {
     }
   }
 
-  log(msg='default msg') {
+  log(msg = 'default msg') {
     console.log(msg);
   }
 

7db5d697ede33f63c952363aa9dff99708aca49d
Jocelo <jocelohere@gmail.com>
2022-02-11 12:42:32 -0600

    New article added to blog landing page

--- src/app/layouts/blog/blog.component.ts
+++ src/app/layouts/blog/blog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-blog',
@@ -16,33 +17,39 @@ export class BlogComponent implements OnInit {
   invalidSearchToken: boolean;
 
   constructor(
-    private router: Router
-  ) {
-
-  }
+    private router: Router,
+    private service: BlogService
+  ) { }
 
   ngOnInit() {
     this.invalidSearchToken = false;
     this.posts = [{
+      url: 'what-is-a-stack',
+      img: 'dishes.png',
+      title: 'What is a stack ?',
+      desc: 'Brief introduction to the concept of a Stack. Heavily used when optimizing algorithms.',
+      publishedDate: this.service.getPostDate('what-is-a-stack'),
+      topics: ['algoritmos', 'pila', 'estructuras de datos']
+    }, {
       url: 'que-es-una-pila',
       img: 'dishes.png',
-      title: 'Que es una pila',
+      title: 'Que es una pila ?',
       desc: 'Breve introducción al concepto de pila en estructuras de datos para resolver algoritmos.',
-      publishedDate: 'June 20, 2021',
+      publishedDate: this.service.getPostDate('que-es-una-pila'),
       topics: ['algoritmos', 'pila', 'estructuras de datos']
     }, {
       url: 'balanced-brackets',
       img: 'blog_balanced_brackets.png',
       title: 'Balanced Brackets',
       desc: 'How to solve the balanced brackets algorithm.',
-      publishedDate: 'June 20, 2021',
+      publishedDate: this.service.getPostDate('balanced-brackets'),
       topics: ['algorithm', 'stack']
     }, {
       url: 'balanceo-de-parentesis',
       img: 'blog_balanced_brackets.png',
       title: 'Balanceo de Parentesis',
       desc: 'Como resolver el problema del balanceo de paréntesis.',
-      publishedDate: 'Junio 20, 2021',
+      publishedDate: this.service.getPostDate('balanceo-de-parentesis'),
       topics: ['algoritmos', 'pila']
     }];
   }

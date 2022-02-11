435a64d7cbd1527f831d593c209e6cb8d7f08a60
Jocelo <jocelohere@gmail.com>
2022-02-11 09:18:46 -0600

    New component: Editor

---
+++ src/app/writer/writer.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { BlogService } from '../services/blog.service';
+
+@Component({
+  selector: 'app-writer',
+  templateUrl: './writer.component.html',
+  styleUrls: ['./writer.component.scss']
+})
+export class WriterComponent implements OnInit {
+  publishedDate: string;
+
+  @Input()
+  lang = 'en';
+
+  constructor(
+    private service: BlogService,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.publishedDate = this.service.getPostDate(this.router.url);
+  }
+
+}

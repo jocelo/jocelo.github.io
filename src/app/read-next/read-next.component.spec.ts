d7f260c7e0e709dc18db8bbf620f9adc3b86b716
Jocelo <jocelohere@gmail.com>
2022-02-11 09:18:34 -0600

    New component: read next

---
+++ src/app/read-next/read-next.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReadNextComponent } from './read-next.component';
+
+describe('ReadNextComponent', () => {
+  let component: ReadNextComponent;
+  let fixture: ComponentFixture<ReadNextComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReadNextComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReadNextComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/read-next/read-next.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BlogService } from '../services/blog.service';
+
+@Component({
+  selector: 'app-read-next',
+  templateUrl: './read-next.component.html',
+  styleUrls: ['./read-next.component.scss']
+})
+export class ReadNextComponent implements OnInit {
+  @Input()
+  lang = 'en';
+
+  nextPosts: any;
+
+  constructor(
+    private service: BlogService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.nextPosts = this.service.getRelatedArticles(this.router.url);
+  }
+
+}

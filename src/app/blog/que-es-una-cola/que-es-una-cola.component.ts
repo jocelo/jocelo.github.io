d645b91ad166757d2780e1e93e52df328639ed3e
Jocelo <jocelohere@gmail.com>
2022-02-22 11:32:04 -0600

    Bug on scope

--- src/app/blog/que-es-una-cola/que-es-una-cola.component.ts
+++ src/app/blog/que-es-una-cola/que-es-una-cola.component.ts
@@ -39,20 +39,20 @@ export class QueEsUnaColaComponent implements OnInit {
     private router: Router,
     private viewportScroller: ViewportScroller
   ) {
-    viewportScroller.setOffset([0, 50]);
+    this.viewportScroller.setOffset([0, 50]);
     router.events.pipe(filter(e => e instanceof Scroll)).subscribe((e: Scroll) => {
       if (e.anchor) {
         // anchor navigation
         /* setTimeout is the core line to solve the solution */
         setTimeout(() => {
-          viewportScroller.scrollToAnchor(e.anchor);
-        })
+          this.viewportScroller.scrollToAnchor(e.anchor);
+        });
       } else if (e.position) {
         // backward navigation
-        viewportScroller.scrollToPosition(e.position);
+        this.viewportScroller.scrollToPosition(e.position);
       } else {
         // forward navigation
-        viewportScroller.scrollToPosition([0, 0]);
+        this.viewportScroller.scrollToPosition([0, 0]);
       }
     });
   }

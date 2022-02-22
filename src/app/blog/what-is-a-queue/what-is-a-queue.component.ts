fe4faa734aeac25b911719050d0a6d35cf94d75f
Jocelo <jocelohere@gmail.com>
2022-02-22 11:32:52 -0600

    Blog post: What is a queue

--- src/app/blog/what-is-a-queue/what-is-a-queue.component.ts
+++ src/app/blog/what-is-a-queue/what-is-a-queue.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
-import * as jsonData from './code.json';
+import { filter } from 'rxjs/operators';
+
+import { faSearch, faLanguage, faTimesCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPython } from '@fortawesome/free-brands-svg-icons';
+import { BlogService } from 'src/app/services/blog.service';
+import { Router, Scroll } from '@angular/router';
+
+import * as code from './code.json';
+
+export interface InextPost {
+  title: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-what-is-a-queue',
@@ -8,10 +21,59 @@ import * as jsonData from './code.json';
   styleUrls: ['./what-is-a-queue.component.scss']
 })
 export class WhatIsAQueueComponent implements OnInit {
+  faLanguage = faLanguage;
+  faTimesCircle = faTimesCircle;
+  faPlusCircle = faPlusCircle;
+  faSearch = faSearch;
+  faPython = faPython;
 
-  constructor() { }
+  theCode: any;
+  codeSteps: any;
+  jsonData: any;
+  pseudoCode: any;
+  pseudoNotes: any;
+
+  nextPosts: InextPost[] = [];
+
+  constructor(
+    private service: BlogService,
+    private router: Router,
+    private viewportScroller: ViewportScroller
+  ) {
+    this.viewportScroller.setOffset([0, 50]);
+    router.events.pipe(filter(e => e instanceof Scroll)).subscribe((e: Scroll) => {
+      if (e.anchor) {
+        // anchor navigation
+        /* setTimeout is the core line to solve the solution */
+        setTimeout(() => {
+          this.viewportScroller.scrollToAnchor(e.anchor);
+        })
+      } else if (e.position) {
+        // backward navigation
+        this.viewportScroller.scrollToPosition(e.position);
+      } else {
+        // forward navigation
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.jsonData = code;
+    this.theCode = {
+      js_code: this.jsonData.javascript.code.join('\n'),
+      py_code: this.jsonData.python.code.join('\n'),
+      php_code: this.jsonData.php.code.join('\n')
+    };
+    this.codeSteps = {
+      'javascript': this.jsonData.javascript.steps,
+      'python': this.jsonData.python.steps,
+      'php': this.jsonData.php.steps
+    };
+    this.pseudoCode = this.jsonData.pseudocode;
+    this.pseudoNotes = this.jsonData.pseudonotes;
+
+    this.nextPosts = this.service.getRelatedArticles(this.router.url);
   }
 
 }

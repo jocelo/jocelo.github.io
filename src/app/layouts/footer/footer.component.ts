c5b90cec2038d8737fc4ca283caa95068063ba81
Jocelo <jocelohere@gmail.com>
2022-05-09 00:05:27 -0400

    Overall style changes

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Icon } from '@fortawesome/fontawesome-svg-core';
+import { faCanadianMapleLeaf, faTwitter, faCodepen, faGithub, faHackerrank, faInstagram, faJsSquare, faLinkedin, faTwitterSquare, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faCopyright, faEnvelope, faExternalLinkAlt, faPepperHot, faProjectDiagram, faSearch, faStar, faUserCircle, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  faCopyright: IconDefinition = faCopyright;
+  faTwitter: IconDefinition = faTwitterSquare;
+
+  faIcons = {
+    codepen: faCodepen,
+    envelope: faEnvelope,
+    exercism: faJsSquare,
+    externallink: faExternalLinkAlt,
+    github: faGithub,
+    hackerrank: faHackerrank,
+    instagram: faInstagram,
+    leaf: faCanadianMapleLeaf,
+    linkedin: faLinkedin,
+    pepperhot: faPepperHot,
+    diagram: faProjectDiagram,
+    search: faSearch,
+    star: faStar,
+    twitter: faTwitter,
+    userCircle: faUserCircle,
+    youtube: faYoutube
+  };
+
+  today: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.today = Date.now();
   }
 
 }
--- src/app/layouts/personal/personal.component.ts
+++ src/app/layouts/personal/personal.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faPepperHot, faStar, faExternalLinkAlt, faUserCircle, faSearch, faEnvelope, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
-import { faCanadianMapleLeaf, faGithub, faCodepen, faLinkedin, faYoutube, faTwitter, faInstagram, faJsSquare, faHackerrank } from '@fortawesome/free-brands-svg-icons';
+import { faStar, faExternalLinkAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-personal',
@@ -9,25 +8,12 @@ import { faCanadianMapleLeaf, faGithub, faCodepen, faLinkedin, faYoutube, faTwit
 })
 export class PersonalComponent implements OnInit {
   faIcons = {
-    codepen: faCodepen,
-    envelope: faEnvelope,
-    exercism: faJsSquare,
     externallink: faExternalLinkAlt,
-    github: faGithub,
-    hackerrank: faHackerrank,
-    instagram: faInstagram,
-    leaf: faCanadianMapleLeaf,
-    linkedin: faLinkedin,
-    pepperhot: faPepperHot,
-    diagram: faProjectDiagram,
-    search: faSearch,
     star: faStar,
-    twitter: faTwitter,
     userCircle: faUserCircle,
-    youtube: faYoutube
   };
 
-  
+
   constructor() { }
 
   ngOnInit() {

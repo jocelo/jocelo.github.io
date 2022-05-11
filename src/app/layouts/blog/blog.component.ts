4d9f15e9efd80d60ff2155d12e0e5554709395ef
Jocelo <jocelohere@gmail.com>
2022-05-11 08:55:32 -0400

    Mobile changes

--- src/app/layouts/blog/blog.component.ts
+++ src/app/layouts/blog/blog.component.ts
@@ -29,6 +29,7 @@ export class BlogComponent implements OnInit {
       imgDesc: 'Photo by <a href="https://unsplash.com/@michalmatlon?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Michal Matlon</a> on <a href="https://unsplash.com/s/photos/math?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>',
       title: 'Two Sum: Blind 75 - 1/75',
       desc: 'We analyze the solution for solving the Two Sum problem using a hashtable; an abstract data type with many use cases. This is the first article of a series dedicated to Blind 75 leetcode algorithms.',
+      lang: 'en',
       readTime: '6',
       publishedDate: this.service.getPostDate('two-sum')
     }, {
@@ -37,6 +38,7 @@ export class BlogComponent implements OnInit {
       imgDesc: 'Photo by <a href="https://unsplash.com/@michalmatlon?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Michal Matlon</a> on <a href="https://unsplash.com/s/photos/math?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>',
       title: 'Suma de dos numeros: Blind 75 - 1/75',
       desc: 'Primer problema listado en el famoso post de Blind: 75 algoritmos de Leetcode. Two sum; Usamos hashtables parar resolver este problema.',
+      lang: 'es',
       readTime: '6',
       publishedDate: this.service.getPostDate('suma-dos-numeros')
     }, {
@@ -45,6 +47,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'Deleting code, one line at a time.',
       desc: 'Tip on how to delete code in VS Code: The shortest way to delete a single line of code in VS Code.',
+      lang: 'en',
       publishedDate: this.service.getPostDate('urls-dinamicos')
     }, {
       url: 'urls-dinamicos',
@@ -52,6 +55,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'URL\s dinámicos.',
       desc: 'Tip de productividad para que le saques el máximo provecho a los sitios que visitas frecuentemente y solo cambia una pequeña parte de la URL. Revisamos las configuraciones necesarias para usarse en Chrome, Firefox y Brave.',
+      lang: 'es',
       publishedDate: this.service.getPostDate('urls-dinamicos')
     }, {
       url: 'format-documents-on-save',
@@ -59,6 +63,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'VSCode tip: Format files on Save.',
       desc: 'Auto-format your files every time you save will help you increase your productivity. You can forget about those small things when coding and focus on your main logic.',
+      lang: 'en',
       publishedDate: this.service.getPostDate('format-documents-on-save')
     }, {
       url: 'what-is-a-queue',
@@ -66,6 +71,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'What is a queue ?',
       desc: 'Brief introduction to the concept of a Queue. Heavily used when optimizing algorithms. Also briefly discussed a more optimal implementation.',
+      lang: 'en',
       publishedDate: this.service.getPostDate('what-is-a-queue')
     }, {
       url: 'que-es-una-cola',
@@ -73,6 +79,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'Que es una cola ?',
       desc: 'Breve introducción al concepto de colas en estructuras de datos para resolver algoritmos. Incluye una breve discusión acerca de una implementación mas optima',
+      lang: 'es',
       publishedDate: this.service.getPostDate('que-es-una-cola')
     }, {
       url: 'what-is-a-stack',
@@ -80,6 +87,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'What is a stack ?',
       desc: 'Brief introduction to the concept of a Stack. Heavily used when optimizing algorithms.',
+      lang: 'en',
       publishedDate: this.service.getPostDate('what-is-a-stack')
     }, {
       url: 'que-es-una-pila',
@@ -87,6 +95,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'Que es una pila ?',
       desc: 'Breve introducción al concepto de pila en estructuras de datos para resolver algoritmos.',
+      lang: 'es',
       publishedDate: this.service.getPostDate('que-es-una-pila')
     }, {
       url: 'balanced-brackets',
@@ -94,6 +103,7 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'Balanced Brackets.',
       desc: 'How to solve the balanced brackets algorithm.',
+      lang: 'en',
       publishedDate: this.service.getPostDate('balanced-brackets')
     }, {
       url: 'balanceo-de-parentesis',
@@ -101,11 +111,14 @@ export class BlogComponent implements OnInit {
       imgDesc: '',
       title: 'Balanceo de Parentesis.',
       desc: 'Como resolver el problema del balanceo de paréntesis.',
+      lang: 'es',
       publishedDate: this.service.getPostDate('balanceo-de-parentesis')
     }];
 
     this.posts.forEach(post => {
       post['topics'] = this.service.getArticleTopics(post['url']);
+      post['flag'] = post['lang'] === 'es' ? 'assets/mx.svg' : 'assets/us.svg';
+      post['lang'] = post['lang'] === 'es' ? 'Español' : 'Ingles';
     });
   }
 

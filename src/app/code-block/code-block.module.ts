bb6b2e5941b4b1211654003d3b20cd514bb1b4ae
Jocelo <jocelohere@gmail.com>
2022-06-05 23:24:41 -0400

    Update in code block component

--- src/app/code-block/code-block.module.ts
+++ src/app/code-block/code-block.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeBlockComponent } from './code-block/code-block.component';
 
@@ -7,14 +7,24 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HighlightModule } from 'ngx-highlightjs';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { LineByLineDialog } from './code-block/code-block.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [CodeBlockComponent],
+  declarations: [CodeBlockComponent, LineByLineDialog],
   imports: [
     CommonModule,
     HighlightModule,
     FontAwesomeModule,
+    MatButtonModule,
     MatTabsModule,
+    MatIconModule,
+    MatDialogModule,
+    MatToolbarModule,
     MatCardModule
   ],
   exports: [
--- src/app/code-block/code-block/code-block.component.ts
+++ src/app/code-block/code-block/code-block.component.ts
@@ -1,9 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Inject } from '@angular/core';
 
 import { faLanguage, faSearch, faPlusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { faJsSquare, faPython, faPhp, faJava } from '@fortawesome/free-brands-svg-icons';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { MatTabChangeEvent } from '@angular/material/tabs';
+
+export interface DialogData {
+  postTitle: string;
+  language: string;
+  code: any;
+  codeSteps: any;
+  pseudoCode: any;
+}
 
 @Component({
   selector: 'app-code-block',
@@ -15,6 +24,7 @@ export class CodeBlockComponent implements OnInit {
   @Input() codeSteps: any;
   @Input() pseudoCode: any;
   @Input() pseudoNotes: any;
+  @Input() postTitle: string;
   @Input() language = 'en';
 
   lineByLine: Boolean = false;
@@ -29,14 +39,66 @@ export class CodeBlockComponent implements OnInit {
   faPython = faPython;
   faPhp = faPhp;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit() {
-    this.lblMode = 'javascript';
+  public openDialog(): void {
+    this.dialog.open(LineByLineDialog, {
+      maxWidth: '98vw',
+      maxHeight: '98vh',
+      height: '98%',
+      data: {
+        postTitle: this.postTitle,
+        language: this.language,
+        code: this.code,
+        codeSteps: this.codeSteps,
+        pseudoCode: this.pseudoCode
+      }
+    });
   }
 
-  public toggleLineByLine(): void {
-    this.lineByLine = !this.lineByLine;
+  ngOnInit() {
+    const firstCode = Object.keys(this.code)[0];
+    this.lblMode = firstCode.split('_')[0];
+  }
+
+  public myTabFocusChange(changeEvent: MatTabChangeEvent): void {
+    this.lblMode = changeEvent.tab.textLabel;
+  }
+}
+
+@Component({
+  selector: 'line-by-line-code-dialog',
+  templateUrl: 'dialog/line-by-line-dialog.html',
+  styleUrls: ['dialog/line-by-line.scss']
+})
+export class LineByLineDialog {
+  lineByLine = false;
+  language: string;
+  code: any;
+  codeSteps: any;
+  pseudoCode: any;
+  pseudoNotes: any;
+  postTitle: string;
+  lblMode: string;
+
+  faTimesCircle = faTimesCircle;
+  faPlusCircle = faPlusCircle;
+  faSearch = faSearch;
+  faLanguage = faLanguage;
+  faJsSquare = faJsSquare;
+  faJava = faJava;
+  faPython = faPython;
+  faPhp = faPhp;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.postTitle = data.postTitle;
+    this.language = data.language;
+    this.code = data.code;
+    this.codeSteps = data.codeSteps;
+    this.pseudoCode = data.pseudoCode;
+
+    const firstCode = Object.keys(data.code)[0];
+    this.lblMode = firstCode.split('_')[0];
   }
 
   public showNotes(theNote): boolean {
@@ -73,5 +135,4 @@ export class CodeBlockComponent implements OnInit {
   public myTabFocusChange(changeEvent: MatTabChangeEvent): void {
     this.lblMode = changeEvent.tab.textLabel;
   }
-
 }

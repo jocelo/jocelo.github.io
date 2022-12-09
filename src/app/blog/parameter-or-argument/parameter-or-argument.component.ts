00dfd02f2889d34b552dc4336af5b6a0efe140c9
Jocelo <jocelohere@gmail.com>
2022-12-09 07:32:29 -0500

    fix: add translated component

--- src/app/blog/parameter-or-argument/parameter-or-argument.component.ts
+++ src/app/blog/parameter-or-argument/parameter-or-argument.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { faYoutube, IconDefinition } from '@fortawesome/free-brands-svg-icons';
 import { faExternalLinkAlt, faLanguage, faSearch } from '@fortawesome/free-solid-svg-icons';
 
@@ -13,14 +14,18 @@ export class ParameterOrArgumentComponent implements OnInit {
   faLanguage: IconDefinition = faLanguage;
   faExternalLink: IconDefinition = faExternalLinkAlt;
 
+  postTitle = 'Parameter or Argument. What is the difference?';
   postPicture: any;
 
-  constructor() { }
+  constructor(
+    private titleService: Title
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle(this.postTitle + ' - Jocelo blog');
     this.postPicture = {
       src: '/assets/blog/altumcode-dC6Pb2JdAqs-unsplash.jpeg',
-      desc: 'Photo by <a href="https://unsplash.com/@altumcode?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">AltumCode</a> en <a href="https://unsplash.com/s/photos/stock?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>'
+      desc: 'Photo by <a href="https://unsplash.com/@altumcode?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">AltumCode</a> on <a href="https://unsplash.com/s/photos/stock?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>'
     };
   }
 
